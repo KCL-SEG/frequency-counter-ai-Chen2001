9b9781bc4c92775ce9866be0c582bbab42801dc3
ACynthiaa <k20101949@kcl.ac.uk>
2022-09-27 20:22:12 +0800

    new version

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,9 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    newItems=[str(x) for x in items]
     frequencies = {}
+    for x in newItems:
+        frequencies[x]=newItems.count(x)
     # Your code goes here
     return frequencies
